3a932e3b3e3d5b0e6ec87c610a4c9e89af254fe3
Clint Hill <clint.hill@gmail.com>
2024-06-22 21:19:02 -0700

    Fix datetime mismatches due to latency in test runs.

--- tests/CloudDataAccessor-metadata.test.ts
+++ tests/CloudDataAccessor-metadata.test.ts
@@ -4,8 +4,8 @@ import { Readable } from "stream";
 import { CloudDataAccessor } from "../src/CloudDataAccessor";
 import { CloudBlobClient } from "../src/CloudBlobClient";
 import { CloudExtensionBasedMapper } from "../src/CloudExtensionBasedMapper";
-import { DC, LDP, NotFoundHttpError, POSIX, RDF, RepresentationMetadata, SOLID_META, XSD, guardStream, toLiteral } from "@solid/community-server";
-import { base, rootFilePath } from "./config";
+import { DC, LDP, NotFoundHttpError, POSIX, RDF, RepresentationMetadata, SOLID_META, XSD, guardStream, isContainerPath, toLiteral } from "@solid/community-server";
+import { base, rootFilePath, within } from "./config";
 
 let mapper = new CloudExtensionBasedMapper(base, rootFilePath);
 let client = new CloudBlobClient(rootFilePath);
@@ -34,8 +34,8 @@ test("Generate the metadata for a resource.", async (t: Test) => {
   t.equal(metadata.contentType, "text/plain", "Content Type.");
   t.equal(metadata.get(RDF.terms.type)?.value, LDP.Resource, "RDF Resource.");
   t.deepEqual(metadata.get(POSIX.terms.size), toLiteral(19, XSD.terms.integer), "POSIX Size.");
-  t.deepEqual(metadata.get(DC.terms.modified), toLiteral(now.toISOString(), XSD.terms.dateTime), "LastModified.");
-  t.deepEqual(metadata.get(POSIX.terms.mtime), toLiteral(Math.floor(now.getTime() / 1000), XSD.terms.integer), "POSIX mtime.");
+  t.ok(within(metadata.get(DC.terms.modified), 2, now, true), "LastModified.");
+  t.ok(within(metadata.get(POSIX.terms.mtime), 2, now), "POSIX mtime.");
   t.equal(metadata.quads(null, null, null, SOLID_META.terms.ResponseMetadata).length, 2, "ResponseMetadata.");
   t.end();
 });
@@ -45,7 +45,7 @@ test("Does not generate size metadata for a container", async (t: Test) => {
   await accessor.writeContainer(identifier, new RepresentationMetadata(identifier));
   let metadata = await accessor.getMetadata(identifier);
   t.notOk(metadata.get(POSIX.terms.size), "No size.");
-  t.deepEqual(metadata.get(DC.terms.modified), toLiteral(now.toISOString(), XSD.terms.dateTime), "LastModified.");
+  t.ok(within(metadata.get(DC.terms.modified), 2, now, true), "LastModified.");
 });
 
 test("Generates the metadata for a container.", async (t: Test) => {
@@ -54,95 +54,69 @@ test("Generates the metadata for a container.", async (t: Test) => {
   t.equal(metadata.identifier.value, identifier.path, "Identifier.");
   t.deepEqual(metadata.getAll(RDF.terms.type), [LDP.terms.Container, LDP.terms.BasicContainer, LDP.terms.Resource], "Container Resource type.");
   t.notOk(metadata.get(POSIX.terms.size), "No size.");
-  t.deepEqual(metadata.get(DC.terms.modified), toLiteral(now.toISOString(), XSD.terms.dateTime), "LastModified.");
-  t.deepEqual(metadata.get(POSIX.terms.mtime), toLiteral(Math.floor(now.getTime() / 1000), XSD.terms.integer), "POSIX mtime.");
+  t.ok(within(metadata.get(DC.terms.modified), 2, now, true), "LastModified.");
+  t.ok(within(metadata.get(POSIX.terms.mtime), 2, now), "POSIX mtime.");
   t.equal(metadata.quads(null, null, null, SOLID_META.terms.ResponseMetadata).length, 1, "ResponseMetadata.");
 });
 
-test("Generates metadata for container child resources.", async (t: Test) => {
+async function createDocument(path: string): Promise<string> {
   let data = guardStream(Readable.from(["data"]));
+  path = `${base}${path}`;
+  let doc = { path };
+  let meta = new RepresentationMetadata(doc);
+  await accessor.writeDocument(doc, data, meta);
+  return path;
+}
+
+test("Generates metadata for container child resources.", async (t: Test) => {
 
   let container = { path: `${base}cloud-data-accessor/container/` };
   await accessor.writeContainer(container, new RepresentationMetadata(container));
-
-  let extraFile = { path: `${base}cloud-data-accessor/container/extraFile` };
-  await accessor.writeDocument(extraFile, data, new RepresentationMetadata(extraFile));
-
-  let resource = { path: `${base}cloud-data-accessor/container/resource` };
-  await accessor.writeDocument(resource, data, new RepresentationMetadata(resource));
-
   let internalContainerOne = { path: `${base}cloud-data-accessor/container/internalContainerOne/` };
   await accessor.writeContainer(internalContainerOne, new RepresentationMetadata(internalContainerOne));
-
-  let shouldNotSeeMe = { path: `${base}cloud-data-accessor/container/internalContainerOne/shouldNotSeeMe` };
-  await accessor.writeDocument(shouldNotSeeMe, data, new RepresentationMetadata(shouldNotSeeMe));
-
   let internalContainerTwo = { path: `${base}cloud-data-accessor/container/internalContainerTwo/` };
   await accessor.writeContainer(internalContainerTwo, new RepresentationMetadata(internalContainerTwo));
 
-  let shouldNotSeeMeEither = { path: `${base}cloud-data-accessor/container/internalContainerTwo/shouldNotSeeMeEither` };
-  await accessor.writeDocument(shouldNotSeeMeEither, data, new RepresentationMetadata(shouldNotSeeMeEither));
+  await createDocument("cloud-data-accessor/container/extraFile");
+  await createDocument("cloud-data-accessor/container/resource");
+  await createDocument("cloud-data-accessor/container/internalContainerOne/shouldNotSeeMe");
+  await createDocument("cloud-data-accessor/container/internalContainerTwo/shouldNotSeeMeEither");
 
   const children: RepresentationMetadata[] = [];
   for await (const child of accessor.getChildren(container)) {
     children.push(child);
   }
 
-  t.equal(children.length, 4, "Resource and it's meta file.");
+  t.equal(children.length, 4, "Children match 4.");
+
+  t.deepLooseEqual(new Set(children.map(c => c.identifier.value)), new Set([
+    `${base}cloud-data-accessor/container/extraFile`,
+    `${base}cloud-data-accessor/container/resource`,
+    `${base}cloud-data-accessor/container/internalContainerOne/`,
+    `${base}cloud-data-accessor/container/internalContainerTwo/`
+  ]), "Identifiers match.");
+
+  for (const c of children.filter(({ identifier }): boolean => identifier.value.endsWith('/'))) {
+    const types = c.getAll(RDF.terms.type).map((t): string => t.value);
+    t.ok(types.includes(LDP.Resource), "Container is Resource.");
+    t.ok(types.includes(LDP.Container), "Container is Container.");
+    t.ok(types.includes(LDP.BasicContainer), "Container is BasicContainer");
+  }
+
+  for (const c of children.filter(({ identifier }): boolean => !identifier.value.endsWith('/'))) {
+    const types = c.getAll(RDF.terms.type).map((t): string => t.value);
+    t.ok(types.includes(LDP.Resource), "Document is Resource.");
+    t.ok(types.includes("http://www.w3.org/ns/iana/media-types/application/octet-stream#Resource"), "Special Case Resource.");
+    t.notOk(types.includes(LDP.Container), "Document is not Container.");
+    t.notOk(types.includes(LDP.BasicContainer), "Document is not BasicContainer");
+  }
+
+  for (const c of children) {
+    t.ok(within(c.get(DC.terms.modified), 2, now, true), "LastModified.");
+    t.ok(within(c.get(POSIX.terms.mtime), 2, now), "POSIX mtime.");
+    t.deepLooseEqual(c.quads(null, null, null, SOLID_META.terms.ResponseMetadata).length,
+      isContainerPath(c.identifier.value) ? 1 : 2, "ResponseMetadata.");
+  }
 });
 
-// it('generates metadata for container child resources.', async(): Promise<void> => {
-//   cache.data = {
-//     container: {
-//       resource: 'data',
-//       'resource.meta': 'metadata',
-//       symlink: Symbol(`${rootFilePath}/container/resource`),
-//       symlinkContainer: Symbol(`${rootFilePath}/container/container2`),
-//       symlinkInvalid: Symbol(`${rootFilePath}/invalid`),
-//       notAFile: 5,
-//       container2: {},
-//     },
-//   };
-
-//   const children = [];
-//   for await (const child of accessor.getChildren({ path: `${base}container/` })) {
-//     children.push(child);
-//   }
-
-//   // Identifiers
-//   expect(children).toHaveLength(4);
-//   expect(new Set(children.map((child): string => child.identifier.value))).toEqual(new Set([
-//     `${base}container/container2/`,
-//     `${base}container/resource`,
-//     `${base}container/symlink`,
-//     `${base}container/symlinkContainer/`,
-//   ]));
-
-//   // Containers
-//   for (const child of children.filter(({ identifier }): boolean => identifier.value.endsWith('/'))) {
-//     const types = child.getAll(RDF.terms.type).map((term): string => term.value);
-//     expect(types).toContain(LDP.Resource);
-//     expect(types).toContain(LDP.Container);
-//     expect(types).toContain(LDP.BasicContainer);
-//   }
-
-//   // Documents
-//   for (const child of children.filter(({ identifier }): boolean => !identifier.value.endsWith('/'))) {
-//     const types = child.getAll(RDF.terms.type).map((term): string => term.value);
-//     expect(types).toContain(LDP.Resource);
-//     expect(types).toContain('http://www.w3.org/ns/iana/media-types/application/octet-stream#Resource');
-//     expect(types).not.toContain(LDP.Container);
-//     expect(types).not.toContain(LDP.BasicContainer);
-//   }
-
-//   // All resources
-//   for (const child of children) {
-//     expect(child.get(DC.terms.modified)).toEqualRdfTerm(toLiteral(now.toISOString(), XSD.terms.dateTime));
-//     expect(child.get(POSIX.terms.mtime))
-//       .toEqualRdfTerm(toLiteral(Math.floor(now.getTime() / 1000), XSD.terms.integer));
-//     // `dc:modified` is in the default graph
-//     expect(child.quads(null, null, null, SOLID_META.terms.ResponseMetadata))
-//       .toHaveLength(isContainerPath(child.identifier.value) ? 1 : 2);
-//   }
-// });
 
--- tests/config.ts
+++ tests/config.ts
@@ -1,5 +1,26 @@
+import type { Term } from '@rdfjs/types';
+import { XSD, toLiteral } from '@solid/community-server';
 
 let base = "http://localhost:9001/";
 let rootFilePath = "test-data";
 
-export { base, rootFilePath };
+/**
+  * This helps to accomodate a 1-2 second diff in times between the Resource creation times,
+  * and the times used in the tests. If there is even the slightest latency on read/writes,
+  * the test datetimes can be off by 1-2 seconds.
+*/
+function within(literal: Term | undefined, s: number, now: Date, isDateStr: boolean = false): boolean {
+  if (!literal) return false;
+  let dateInt = toLiteral(Math.floor(now.getTime() / 1000), XSD.terms.integer);
+  let dateString = toLiteral(now.toISOString(), XSD.terms.dateTime);
+  let delta: number;
+
+  if(isDateStr) {
+    delta = parseInt(dateString.value) - parseInt(literal?.value);
+  } else {
+    delta = parseInt(dateInt.value) - parseInt(literal?.value);
+  }
+  return delta <= s;
+}
+
+export { base, rootFilePath, within };
